6941748eb2d9f495a2cf325c6a8cd4c08cea7ebd
eeng4657 <eeng4657@bths.edu>
2022-09-29 12:03:55 -0400

    Added comments

--- src/U2L3Runner.java
+++ src/U2L3Runner.java
@@ -10,6 +10,7 @@ Box box1 = new Box(length, width, height);
 Box box2 = new Box(length);
 box1.printDimensions();
 box2.printDimensions();
+// Prints the dimensions of box1, a rectangle, and box2, a square
 
 String bigger;
 
@@ -25,15 +26,15 @@ else
 {
   bigger = "both boxes have equal volume";
 }
+// Determines which box is bigger or if they have equal volume
 
 System.out.println(bigger);
 
 boolean box1sides = box1.anySideLongerThan((int)length);
 boolean box2sides = box2.anySideLongerThan((int)length);
+// Determines if any side of the given box is longer than a given length.
 
 System.out.println("box1 has a side that exceeds " + length + ": " + box1sides);
 System.out.println("box2 has a side that exceeds " + length + ": " + box2sides);
-
-
     }
 }
